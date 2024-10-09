d2caba70643b3d0a43c99870fe4e6f80a4cad3eb
ilia-pw <iliabelkania91@gmail.com>
2024-10-09 21:23:44 +0400

    second commit

---
+++ day09/classwork/lessone1.py
@@ -0,0 +1,12 @@
+print(int(input("შეიყვანეთ თქვენი ასაკი:")))
+age=13
+print(age <= 18)
+
+
+print(input("შეიყვანეთ თქვენი სახელი: "))
+name = "ilia"
+print("ilia" <= "ilia")
+
+print(int(input("შეიყვანეთ თქვენი ასაკი: ")))
+age=20
+print(age / 2)
---
+++ day09/homework/lessone1.py
@@ -0,0 +1,18 @@
+print(input("შეიყვანეთ თქვენი ასაკი: "))
+age = 13
+print(age // 10)
+
+print(input("შეიყვანეთ თქვენი სახელი: "))
+name = "ilia"
+print(name <= "ilia")
+
+
+print(input("შეიყვანეთ თქვენი ასაკი: "))
+age = 13
+print(input("შეიყვანეთ თქვენი ბალანსი: "))
+balance = 1000
+print(1000 % 13)
+
+print(input("შეიყვანეთ თქვენი ასაკი: "))
+age = 13
+print(13 / 2)
