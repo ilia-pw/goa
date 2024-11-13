aac01649baf5273bfe1384473d23b2c08a2061d0
ilia-pw <iliabelkania91@gmail.com>
2024-11-13 17:47:36 +0400

    second commit

---
+++ day17/classwork/lessone1.py
@@ -0,0 +1,6 @@
+names = ["ilia", "dato", "vano", "misho", "nika", "mari", "giorgi"]
+
+for i in names:
+    if i == "giorgi":
+        print(i)
+
---
+++ day17/homework/lessone1.py
@@ -0,0 +1,36 @@
+names = ["ilia", "vano", "data", "nika", "misho"]
+
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+
+
+numbers = [1,2,"ilia",4,5,6,"vano",8,9,10]
+
+for i in range(10):
+    print(numbers[i])
+
+for i in numbers:
+    print(i)
+
+
+numbers = [1,5,10,15,6]
+
+for i in numbers:
+    if i > 10:
+        print(i)
+
+
+
+random_items = ["ilia", 15, 15.5, True, False,"25","True"]
+
+for i in random_items:
+    print(type(i))
+
+
+
+
+
+ 
