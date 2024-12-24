ee41c07a4dfa2b90bf51ee052fcd0d9599fcdce9
ilia-pw <iliabelkania91@gmail.com>
2024-12-24 21:48:00 +0400

    second commit

---
+++ day31/classwork/lessone1.py
@@ -0,0 +1,33 @@
+
+# Reversed Strings
+def remove_char(s):
+    return s[1:-1]
+
+
+
+# Convert a String to a Number!
+def remove_char(s):
+    return s[1:-1]
+
+
+# Convert a Boolean to a String
+def boolean_to_string(b):
+    return str(b)
+
+
+
+# MakeUpperCase
+def make_upper_case(s):
+    return s.upper()
+
+# Convert boolean values to strings 'Yes' or 'No'.
+def bool_to_word(boolean):
+    if boolean == True:
+        return "Yes"
+    else:
+        return "No"
+    
+
+# Remove First and Last Character
+def remove_char(s):
+    return s[1:-1]
---
+++ day31/homework/lessone1.py
@@ -0,0 +1,11 @@
+
+# Function 1 - hello world
+def greet():
+    return  "hello world!"
+
+
+# Opposite number
+def opposite(number):
+    return -number
+  
+# ver gavakete
